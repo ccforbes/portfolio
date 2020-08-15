1fe0a1e579b8769210e84784930883ef04987042
ccforbes <ccforbes1998@gmail.com>
2020-08-14 18:08:06 -0700

    added some comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import App from 'next/app'
 
+import "../styles/styles.css" // used for global styling
+
+// used for font-awesome icon styling in SSR
 import { config } from '@fortawesome/fontawesome-svg-core'
-import "../styles/styles.css"
 import "@fortawesome/fontawesome-svg-core/styles.css"
 config.autoAddCss = false
-
 
 export default App
